1bd3b1b04fe26c703d35d1fab95e008eb147d1ac
Anton C. Gabriel <anton.pettersson.privat@gmail.com>
2024-11-27 23:04:11 +0100

    Employee class created
    Created parent class Employee and tested it in Main

--- OOP Assignment-2/src/Main.java
+++ OOP Assignment-2/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Employee myEmployee = new Employee("Greger");
+        Employee myEmployee2 = new Employee("Greg");
     }
 }
